8e764622cb17bfbb6d950f1a4b4a57545fbca83f
Mar <mar.gonzalezmontesinos@studentmail.unicas.it>
2025-12-01 16:23:56 +0100

    Change MySQL password to a new value
    Updated the database password for MySQL connection.

---
+++ db/dls_schema.sql
@@ -0,0 +1,222 @@
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dls_schema
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `genre`
+--
+
+DROP TABLE IF EXISTS `genre`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `genre` (
+  `idGenre` int NOT NULL AUTO_INCREMENT,
+  `genre` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idGenre`)
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `genre`
+--
+
+LOCK TABLES `genre` WRITE;
+/*!40000 ALTER TABLE `genre` DISABLE KEYS */;
+INSERT INTO `genre` VALUES (1,'romance'),(2,'fiction'),(3,'fantasy'),(4,'horror'),(5,'adventure'),(6,'drama'),(7,'history'),(8,'science'),(9,'children'),(10,'blues'),(11,'jazz'),(12,'rock'),(13,'pop'),(14,'reggae'),(15,'opera'),(16,'kpop'),(17,'news'),(18,'fashion'),(19,'sports'),(20,'cooking'),(21,'fitness'),(22,'home and garden'),(23,'romance'),(24,'fiction'),(25,'fantasy'),(26,'horror'),(27,'adventure'),(28,'drama'),(29,'history'),(30,'science'),(31,'children'),(32,'blues'),(33,'jazz'),(34,'rock'),(35,'pop'),(36,'reggae'),(37,'opera'),(38,'kpop'),(39,'news'),(40,'fashion'),(41,'sports'),(42,'cooking'),(43,'fitness'),(44,'home and garden');
+/*!40000 ALTER TABLE `genre` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `holds`
+--
+
+DROP TABLE IF EXISTS `holds`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `holds` (
+  `idHold` int NOT NULL AUTO_INCREMENT,
+  `idUser` int NOT NULL,
+  `idMaterial` int NOT NULL,
+  `hold_date` datetime(2) NOT NULL,
+  `hold_status` varchar(45) NOT NULL,
+  PRIMARY KEY (`idHold`),
+  KEY `idUser_idx` (`idUser`),
+  KEY `idMaterial_idx` (`idMaterial`),
+  CONSTRAINT `idMaterial_FK` FOREIGN KEY (`idMaterial`) REFERENCES `materials` (`idMaterial`),
+  CONSTRAINT `idUser_FK` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `holds`
+--
+
+LOCK TABLES `holds` WRITE;
+/*!40000 ALTER TABLE `holds` DISABLE KEYS */;
+/*!40000 ALTER TABLE `holds` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `loans`
+--
+
+DROP TABLE IF EXISTS `loans`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `loans` (
+  `idLoan` int NOT NULL AUTO_INCREMENT,
+  `idUser` int NOT NULL,
+  `idMaterial` int NOT NULL,
+  `start_date` datetime(2) NOT NULL,
+  `due_date` datetime(2) NOT NULL,
+  `return_date` datetime(2) DEFAULT NULL,
+  PRIMARY KEY (`idLoan`),
+  KEY `idUser_idx` (`idUser`),
+  KEY `idMaterial_idx` (`idMaterial`),
+  CONSTRAINT `idMaterial_FK2` FOREIGN KEY (`idMaterial`) REFERENCES `materials` (`idMaterial`),
+  CONSTRAINT `idUser_FK2` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `loans`
+--
+
+LOCK TABLES `loans` WRITE;
+/*!40000 ALTER TABLE `loans` DISABLE KEYS */;
+/*!40000 ALTER TABLE `loans` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `material_type`
+--
+
+DROP TABLE IF EXISTS `material_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `material_type` (
+  `idMaterialType` int NOT NULL AUTO_INCREMENT,
+  `material_type` varchar(45) NOT NULL,
+  PRIMARY KEY (`idMaterialType`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `material_type`
+--
+
+LOCK TABLES `material_type` WRITE;
+/*!40000 ALTER TABLE `material_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `material_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `materials`
+--
+
+DROP TABLE IF EXISTS `materials`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `materials` (
+  `idMaterial` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  `author` varchar(45) DEFAULT NULL,
+  `year` int DEFAULT NULL,
+  `ISBN` varchar(45) DEFAULT NULL,
+  `idGenre` int DEFAULT NULL,
+  `idMaterialType` int DEFAULT NULL,
+  `material_status` varchar(45) NOT NULL,
+  PRIMARY KEY (`idMaterial`),
+  KEY `idGenre_idx` (`idGenre`),
+  KEY `idMaterialType_idx` (`idMaterialType`),
+  CONSTRAINT `idGenre_FK` FOREIGN KEY (`idGenre`) REFERENCES `genre` (`idGenre`),
+  CONSTRAINT `idMaterialType_FK` FOREIGN KEY (`idMaterialType`) REFERENCES `material_type` (`idMaterialType`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `materials`
+--
+
+LOCK TABLES `materials` WRITE;
+/*!40000 ALTER TABLE `materials` DISABLE KEYS */;
+/*!40000 ALTER TABLE `materials` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `idRole` int NOT NULL AUTO_INCREMENT,
+  `admin_type` varchar(45) NOT NULL,
+  PRIMARY KEY (`idRole`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `idUser` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `surname` varchar(45) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `nationalID` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `idRole` int NOT NULL,
+  PRIMARY KEY (`idUser`),
+  KEY `isAdmin_idx` (`idRole`),
+  CONSTRAINT `idRole_FK` FOREIGN KEY (`idRole`) REFERENCES `roles` (`idRole`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-12-01 16:23:05
