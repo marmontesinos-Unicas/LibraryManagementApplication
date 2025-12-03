5ed7f81a1eebaf67c7d17b8828933209b550a79f
ferra <ferran.montero@studentmail.unicas.it>
2025-12-03 14:29:59 +0100

    Rebuild of the project

--- test/it/unicas/project/template/address/service/MaterialServiceTest.java
+++ test/it/unicas/project/template/address/service/MaterialServiceTest.java
@@ -1,6 +1,5 @@
 package it.unicas.project.template.address.service;
 
-import it.unicas.project.template.address.service.MaterialService;
 import it.unicas.project.template.address.FakeMaterialDAO;
 import it.unicas.project.template.address.model.Material;
 import it.unicas.project.template.address.model.dao.DAOException;
